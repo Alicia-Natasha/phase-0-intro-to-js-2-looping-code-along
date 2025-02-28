c608c57eb8eaff400c05f88f2bb7728e1e994f03
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-02-28 18:31:11 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards() {
+    return [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+         "Thank you, Aki, for the wonderful surprise gift!",
+    ];
+}
+
+function countDown(number) {
+    while (number >= 0) { 
+        console.log(number);
+        number--;
+    }
+}
+
+
